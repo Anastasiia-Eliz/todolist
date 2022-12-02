aa757bfc1f4f09742c71c0026938545368fd3a81
Anastasiia-Eliz <aselizarova@yandex.ru>
2022-12-03 00:30:27 +0300

    authorization 3

--- todolist/urls.py
+++ todolist/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls'), name='core'),
-    path('api/oauth/', include('social_django.urls', namespace='social')),
+    path('core/', include('core.urls'), name='core'),
+    path('oauth/', include('social_django.urls', namespace='social')),
 ]
 
 
