e291400911180230446f1398d60913d1b17f3b48
Anastasiia-Eliz <aselizarova@yandex.ru>
2022-12-15 18:38:19 +0300

    update database

--- goals/views.py
+++ goals/views.py
@@ -134,34 +134,42 @@ class BoardCreateView(CreateAPIView):
 	serializer_class = BoardCreateSerializer
 
 
-class BoardView(RetrieveUpdateDestroyAPIView):
-	model = Board
-	permission_classes = [permissions.IsAuthenticated, BoardPermissions]
-	serializer_class = BoardSerializer
-
-	def get_queryset(self):
-		return Board.objects.filter(participants__user=self.request.user, is_deleted=False)
-
-	def perform_destroy(self, instance: Board):
-		with transaction.atomic():
-			instance.is_deleted = True
-			instance.save()
-			instance.categories.update(is_deleted=True)
-			Goal.objects.filter(category__board=instance).update(
-				status=Goal.Status.archived
-			)
-		return instance
-
-
 class BoardListView(ListAPIView):
-	model = Board
-	permission_classes = [permissions.IsAuthenticated]
-	pagination_class = LimitOffsetPagination
-	serializer_class = BoardListSerializer
-	filter_backends = [
-		filters.OrderingFilter,
-	]
-	ordering = ["title"]
+    model = Board
+    permission_classes = [permissions.IsAuthenticated, BoardPermissions]
+    serializer_class = BoardListSerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = [
+        filters.OrderingFilter,
+    ]
+    ordering_fields = ["title"]
+    ordering = ["title"]
 
-	def get_queryset(self):
-		return Board.objects.filter(participants__user=self.request.user, is_deleted=False)
+    def get_queryset(self):
+        return Board.objects.filter(
+            participants__user=self.request.user, is_deleted=False
+        )
+
+
+class BoardView(RetrieveUpdateDestroyAPIView):
+    model = Board
+    permission_classes = [permissions.IsAuthenticated, BoardPermissions]
+    serializer_class = BoardSerializer
+
+    def get_queryset(self):
+        return Board.objects.filter(
+            participants__user=self.request.user, is_deleted=False
+        )
+
+    def perform_destroy(self, instance: Board):
+        # При удалении доски помечаем ее как is_deleted,
+        # «удаляем» категории, обновляем статус целей
+        with transaction.atomic():
+            instance.is_deleted = True
+            instance.save()
+            instance.categories.update(is_deleted=True)
+            Goal.objects.filter(category__board=instance).update(
+                status=Goal.Status.archived
+            )
+        return instance
+
